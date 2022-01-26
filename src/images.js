a467b5cdeabfc58a940fa67a35baa3f076021a73
Glen Yang <gleny4001@gmail.com>
2022-01-26 22:34:55 +0900

    Fix image path

--- src/images.js
+++ src/images.js
@@ -1,12 +1,12 @@
-import productImg1 from "/src/images/products/product-1.jpg";
-import productImg2 from "/src/images/products/product-2.jpg";
-import productImg3 from "/src/images/products/product-3.jpg";
-import productImg4 from "/src/images/products/product-4.jpg";
-import productImg5 from "/src/images/products/product-5.jpg";
-import productImg6 from "/src/images/products/product-6.jpg";
-import productImg7 from "/src/images/products/product-7.jpg";
-import productImg8 from "/src/images/products/product-8.jpg";
-import productImg9 from "/src/images/products/product-9.jpg";
+import productImg1 from "./images/products/product-1.jpg";
+import productImg2 from "./images/products/product-2.jpg";
+import productImg3 from "./images/products/product-3.jpg";
+import productImg4 from "./images/products/product-4.jpg";
+import productImg5 from "./images/products/product-5.jpg";
+import productImg6 from "./images/products/product-6.jpg";
+import productImg7 from "./images/products/product-7.jpg";
+import productImg8 from "./images/products/product-8.jpg";
+import productImg9 from "./images/products/product-9.jpg";
 
 export default [
   productImg1,
